adcf69ba03d50aff72278407a1cc1dae0fffbd67
Dimitar Yanchev <dimitar.yanchev@acronis.com>
2022-04-22 09:25:54 +0300

    Implement migrations and models

---
+++ app/Models/Feed.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Feed extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string
+     */
+    public $table = 'feeds';
+
+    /**
+     * @var string[]
+     */
+    public $fillable = [
+        "title",
+        "source",
+        "source_url",
+        "publish_date",
+        "description"
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Rss::class, 'rss_id');
+    }
+}
