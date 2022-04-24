c23d85c438eeae69aa8da4b07cff33c3a2db63a7
Dimitar Yanchev <dimitar.yanchev@acronis.com>
2022-04-24 10:03:24 +0300

    Add rbbitmq lib

--- config/app.php
+++ config/app.php
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        NeedleProject\LaravelRabbitMq\Providers\ServiceProvider::class,
 
     ],
 
